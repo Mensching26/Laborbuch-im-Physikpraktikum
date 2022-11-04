39935fbe0fc93a9f7321e71e85852967a4d06ef8
Justus Weyers <justus.weyers@uni-potsdam.de>
2022-11-04 12:55:20 +0100

    renamed:    "Arbeitsbl\303\244tter/Arbeitsblatt_Messunsicherheiten_Teil_1_v2.ipynb" -> "Arbeitsbl\303\244tter/Arbeitsblatt_Messunsicherheiten_Teil_1.ipynb" 	deleted:    "Arbeitsbl\303\244tter/Arbeitsblatt_Messunsicherheiten_Teil_2.ipynb" 	new file:   Hausaufgaben/Woche1/.Rhistory 	new file:   Hausaufgaben/Woche1/PHHA1.Rmd 	new file:   Hausaufgaben/Woche1/PHHA1.aux 	new file:   Hausaufgaben/Woche1/PHHA1.log     new file:   Hausaufgaben/Woche1/PHHA1.out 	new file:   Hausaufgaben/Woche1/PHHA1.pdf 	new file:   Hausaufgaben/Woche1/PHHA1.tex 	new file:   Hausaufgaben/Woche1/PHHA1_Word.pdf 	new file:   Hausaufgaben/Woche1/rmdgoonline.R 	renamed:    INPUT/Bodenkundedaten.txt -> INPUT/Messreihen/Bodenkundedaten.txt 	renamed:    Kopie_von_Arbeitsblatt_Messunsicherheiten_Teil_1_v2.ipynb -> Veraltet/Kopie_von_Arbeitsblatt_Messunsicherheiten_Teil_1_v2.ipynb 	renamed:    Test.ipynb -> Veraltet/Test.ipynb 	renamed:    Tools.ipynb -> Veraltet/Tools.ipynb

---
+++ Hausaufgaben/Woche1/rmdgoonline.R
@@ -0,0 +1,5 @@
+#install.packages('trackdown')
+
+library(trackdown)
+upload_file(file='C:/Users/justu/Desktop/gogogo.Rmd',
+            gpath='https://drive.google.com/drive/my-drive/rmdphysikpraktikum')
